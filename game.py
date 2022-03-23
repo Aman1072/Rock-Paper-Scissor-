99d15e1d0b24adbc409f73165ac9cd5d9bb74980
Aman1072 <amandesai10723310@gmail.com>
2022-03-23 05:59:24 +0530

    change in some feature

--- game.py
+++ game.py
@@ -12,41 +12,95 @@ player_lives = 3
 computer_lives = 3
 total_lives = 3
 
-#Version 1, to explain array indexing
-#player_chioce = choices[1]
-#print("index 1 in the chice array is " + player_chioce + ", which is paper")
+# True or false are boolean data types 
+# essentially, booleans are equivalent to an on or off switch, 1 or 0.
+player_choice = False
 
-player_chioce = input("Choose rock, paper, scissors: ")
+# plyer_chioce == False
+while player_choice is False:
+    #Version 1, to explain array indexing
+    #player_choice = choices[1]
+    #print("index 1 in the chice array is " + player_choice + ", which is paper")
 
-print("user chose" + player_chioce)
+    player_choice = input("Choose rock, paper, scissors: ")
+    #player_choice now equals TRUE -> it has a value
 
-# this will be the AI choice -> a random pick from the choices array 
-computer_chioce = choices [randint(0,2)]
+    print("user chose:" + player_choice)
 
-print("computer chose: " + computer_chioce)
+    # this will be the AI choice -> a random pick from the choices array 
+    computer_choice = choices[randint(0,2)]
 
-if computer_chioce == player_chioce:
-    print("tie")
+    print("computer chose: " + computer_choice)
 
-elif computer_chioce == "rock":
-    if player_chioce == "scissors":
-        print("you lose!")
-        #verbose way
-        # player_lives = plyer_lives - 1
-        #simplified way
-        player_lives -= 1
+    if computer_choice == player_choice:
+        print("tie")
 
-elif computer_chioce == "paper":
-    if player_chioce == "scissors":
-        print("you win!")
-        computer_lives -= 1
+    elif computer_choice == "rock":
+        if player_choice == "scissors":
+            print("you lose!")
+            #verbose way
+            # player_lives = plyer_lives - 1
+            #simplified way
+            player_lives -= 1
+        else:
+            print("you win!")
+            computer_lives -= 1    
 
-elif computer_chioce == "scissors":
-    if player_chioce == "paper":
-        print("you lose!")
-        player_lives -= 1
+    elif computer_choice == "paper":
+        if player_choice == "scissors":
+            print("you win!")
+            computer_lives -= 1
+        else:
+            print("you lose!")
+            player_lives -= 1    
 
-print("Player lives:", player_lives)
-print("Computer lives:", computer_lives)
+    elif computer_choice == "scissors":
+        if player_choice == "paper":
+            print("you lose!")
+            player_lives -= 1
+        else:
+            print("you win")
+            computer_lives -= 1    
+
+    if player_lives == 0:
+        print("you lose! Would you like to play again?")
+        choice = input("Y / N? ")
+
+        if choice == "N" or choice == "n":
+            print("You chose to quite! Better luck next time!")
+            exit()
+        elif choice == "Y" or choice == "y":
+            #reset the player lives and computer lives 
+            #and reste player choices to False, so our loop restarts
+            player_lives = total_lives
+            computer_lives = total_lives
+        else:
+            print("Make a valid choice - Y or N")
+            #this might generate a bug that we need to fix later 
+            choice = input("Y / N?")         
+
+    if computer_lives == 0:
+        print("you win! Would you like to play again?")
+        choice = input("Y / N ? ")
+
+        if choice == "N" or choice == "n":
+            print("You chose to quite! Better luck next time!")
+            exit()
+        elif choice == "Y"  or choice == "y":
+            #reset the player lives and computer lives 
+            #and reste player choices to False, so our loop restarts
+            player_lives = total_lives
+            computer_lives = total_lives
+        else:
+            print("Make a valid choice - Y or N")
+            #this might generate a bug that we need to fix later 
+            choice = input("Y / N?")                     
+
+    print("Player lives:", player_lives)
+    print("Computer lives:", computer_lives)
+
+    # map the loop keep running, by setting player_cioce back to False 
+    # unset, so that our loop condition will evaluate to True
+    player_chioce = False 
 
 
